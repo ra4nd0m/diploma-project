6ff47aac7b96a4e86303ac89d2bb22b9bd3d335b
Kosyakov Egor <58385866+ra4nd0m@users.noreply.github.com>
2026-04-04 00:37:42 +0300

    feat: add chi router and achievement routes for handling achievements

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module achievement-service
 go 1.25.0
 
 require (
+	github.com/go-chi/chi/v5 v5.2.5
 	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.9.1
---
+++ internal/routes/router.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"achievement-service/internal/handlers"
+	"achievement-service/internal/middleware"
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func NewRouter(
+	logger *slog.Logger,
+	achievementHandler *handlers.AchievementHandler,
+	authMiddleware func(http.Handler) http.Handler,
+) http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.NewRequestLogMiddleware(logger))
+
+	r.Get("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	r.Group(func(r chi.Router) {
+		r.Use(authMiddleware)
+
+		r.MethodFunc(http.MethodGet, "/achievements", achievementHandler.GetAchievements)
+		r.MethodFunc(http.MethodPost, "/achievements", achievementHandler.CreateAchievement)
+		r.MethodFunc(http.MethodPost, "/achievements/issue", achievementHandler.IssueAchievement)
+		r.MethodFunc(http.MethodGet, "/achievements/{achievementID}", achievementHandler.GetAchievementByID)
+	})
+
+	return r
+}
